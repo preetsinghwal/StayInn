60ea8f9a429e6ebe0f806a01d2ac39bd1f2d2e87
Preet Singhwal <preetsinghwal38@gmail.com>
2025-07-24 02:01:45 +0530

    added some starter files, fetch dummy data in server component of cabin

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,10 @@
-export default function Page() {
+export default async function Page() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
     return (
-        <h3>Cabins Page</h3>
+        <div>
+            <h3>Cabins Page</h3>
+            <ul>{data.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+        </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1>Welcome to StayInn Hotel</h1>
       <Link href="/cabins">Explore luxary rooms</Link>
     </div>
