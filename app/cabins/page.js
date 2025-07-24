5ad872ec3ae4e586c41307c32581ce0917caeab6
Preet Singhwal <preetsinghwal38@gmail.com>
2025-07-25 01:58:24 +0530

    added dummy loading, client comp pages

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,5 @@
+import Counter from "../components/Counter";
+
 export default async function Page() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
@@ -5,6 +7,7 @@ export default async function Page() {
         <div>
             <h3>Cabins Page</h3>
             <ul>{data.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+            <Counter users={data}/>
         </div>
     )
 }
