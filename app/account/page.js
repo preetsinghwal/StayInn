6a7d99ee35248a4882a305a581da7390f45ca61c
Preet Singhwal <preetsinghwal38@gmail.com>
2025-07-26 03:07:52 +0530

    Added cabin, about, account and it's nested route(profile, reservations) routes pages with styles, some dependency, etc

--- app/account/page.js
+++ app/account/page.js
@@ -1,5 +1,11 @@
+export const metadata = {
+  title: "Guest Area",
+};
+
 export default function Page() {
-    return (
-        <h3>Account Page</h3>
-    )
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welome Preet
+    </h2>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,34 @@
-import Logo from "./components/Logo"
-import Navigation from "./components/Navigation"
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "Stay Inn"
-}
+  title: {
+    template: "%s | The Stay Inn",
+    default: "Welcome | The Stay Inn",
+  },
+  description:
+    "Luxurious cabin hotel, location in the heart of the himalayas, surrounded by beautiful himalyan range mountains and breath taking views",
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by StayInn @2025</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
-  )
+  );
 }
