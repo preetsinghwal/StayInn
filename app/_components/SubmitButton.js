435b5afd6864bfe0fafe333411f6fa0411003e95
Preet Singhwal <preetsinghwal38@gmail.com>
2025-08-13 01:03:29 +0530

    added edit reservation functionality, added server action corresponding to that as well

---
+++ app/_components/SubmitButton.js
@@ -0,0 +1,16 @@
+"use client";
+
+const { useFormStatus } = require("react-dom");
+
+export default function SubmitButton({children, pendingLabel}) {
+    //This comp needs to be rendered inside a form and it should be a client comp
+    const { pending } = useFormStatus();
+    return (
+      <button
+        className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+        disabled={pending}
+      >
+        {pending ? pendingLabel: children}
+      </button>
+    );
+  }
